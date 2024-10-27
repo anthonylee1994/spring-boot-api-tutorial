0eb30ecedaf856868cf11332a32c66b4b42a6e2a
Anthony Lee <anthonylee1994@users.noreply.github.com>
2024-10-27 19:53:55 +0800

    Refactor CompanyClient to use path variable in getCompanyById method

--- job-service/src/main/java/com/learn/jobservice/job/clients/CompanyClient.java
+++ job-service/src/main/java/com/learn/jobservice/job/clients/CompanyClient.java
@@ -11,5 +11,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "COMPANY-SERVICE", url = "${company-service.url}")
 public interface CompanyClient {
     @GetMapping("/companies/{id}")
-    Company getCompanyById(@PathVariable Long id);
+    Company getCompanyById(@PathVariable("id") Long id);
 }
